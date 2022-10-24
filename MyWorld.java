06ecd315d2d29ddc49c7dac653a3ca471b16d17c
LAFLAME55 <serkan.tugra@swisscom.com>
2022-10-24 20:17:23 +0200

    Changed world settings

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,8 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 800x800 cells with a cell size of 1x1 pixels. Background color set to light purple with a .png.
+        super(800, 800, 1); 
+        setBackground("light-purple.png");
     }
 }
