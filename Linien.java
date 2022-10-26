f308812b26c2bd38d43b772e5ca56517132c53c2
LAFLAME55 <serkan.tugra@swisscom.com>
2022-10-26 23:13:07 +0200

    Created start field and added pictures to library

---
+++ Linien.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Linien here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Linien extends Actor
+{
+    /**
+     * Act - do whatever the Linien wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -13,10 +13,24 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    // Ruft alle Methoden auf
     public MyWorld()
     {    
         // Create a new world with 800x800 cells with a cell size of 1x1 pixels. Background color set to light purple with a .png.
-        super(800, 800, 1); 
-        setBackground("light-purple.png");
+        super(3, 3, 200); 
+        Lines();
+        // setBackground("light-purple.png");
     }
+    // Generiert schwarze Linien und platziert diese dynamisch in der Welt.
+    public void Lines() {
+        GreenfootImage backgroundimage = getBackground();
+        int i = 1;
+        while (i < 3) {
+            backgroundimage.setColor(Color.BLACK);
+            backgroundimage.drawLine(i*200, 0, i*200, 600);
+            backgroundimage.drawLine(0, i*200, 600, i*200);
+            i = i + 1;
+        }
+    }
+    
 }
