7c75e69c01162591401ea6986f09f311c861d733
LAFLAME55 <serkan.tugra@swisscom.com>
2022-10-31 20:14:54 +0100

    Added end game if field full function

--- TicTacToeULTRAPROMAX.java
+++ TicTacToeULTRAPROMAX.java
@@ -9,37 +9,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TicTacToeULTRAPROMAX extends World
 {
     private int mouseclicked; // Überprüft, wie viel Mal die Maus geklickt wurde
-    private Actor Icon; 
-    private boolean playerturn; // Überprüft, welcher Spieler am Zug ist (true=Spieler1/false=Spieler2)
-    private String[][] fielddeclaration = {{"", "", ""},
-                                           {"", "", ""},
-                                           {"", "", ""}};
     private boolean checkXTurnStatus;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    // Ruft alle Methoden auf
+    // Ruft Methoden für das Erstellen des Spielfelds auf. Erstellt eine neue Welt mit 3x3 Feld à 200 px
     public TicTacToeULTRAPROMAX() {    
-        // Create a new world with 800x800 cells with a cell size of 1x1 pixels. Background color set to light purple with a .png.
         super(3, 3, 200); 
         createFields();
-        // setLines();
         checkXTurnStatus = true; 
-        // setBackground("light-purple.png");
     }
-    // Generiert schwarze Linien und platziert diese dynamisch in der Welt.
-    // public void setLines() {
-        // GreenfootImage backgroundimage = getBackground();
-        // int i = 1;
-        // while (i < 3) {
-            // backgroundimage.setColor(Color.BLACK);
-            // backgroundimage.drawLine(i*200, 0, i*200, 600);
-            // backgroundimage.drawLine(0, i*200, 600, i*200);
-            // i = i + 1;
-        // }
-    // }
-    // 
+
     public void createFields() {
         for(int x = 0; x < 3; x++) {
             for(int y = 0; y < 3; y++) {
@@ -57,44 +38,62 @@ public class TicTacToeULTRAPROMAX extends World
     }
 
     public void act() {
-        // Funktion überprüft, ob YE gewonnen hat
+        // Funktionsblock überprüft, ob YE gewonnen hat
         for(int x = 0; x < 3; x++) {
             for(int y = 0; y < 3; y++) {
+                // Überprüft, ob eine Reihe auf X-Achse voll ist
                 if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x + 1, y, ye.class).size() > 0 && getObjectsAt(x + 2, y, ye.class).size() > 0) {
                         System.out.println("YE hat gewonnen!");
                         Greenfoot.stop();
                 } 
-                if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x, y + 1, ye.class).size() > 0 && getObjectsAt(x, y + 2, ye.class).size() > 0) {
+                // Überprüft, ob eine Reihe auf Y-Achse voll ist
+                else if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x, y + 1, ye.class).size() > 0 && getObjectsAt(x, y + 2, ye.class).size() > 0) {
                         System.out.println("YE hat gewonnen!");
                         Greenfoot.stop();
                 } 
-                if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x + 1, y + 1, ye.class).size() > 0 && getObjectsAt(x + 2, y + 2, ye.class).size() > 0) {
+                // Überprüft, ob eine Reihe quer (XY-Achse) voll ist
+                else if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x + 1, y + 1, ye.class).size() > 0 && getObjectsAt(x + 2, y + 2, ye.class).size() > 0) {
                         System.out.println("YE hat gewonnen!");
                         Greenfoot.stop();
                 } 
-                if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x + 1, y - 1, ye.class).size() > 0 && getObjectsAt(x + 2, y - 2, ye.class).size() > 0) {
+                else if(getObjectsAt(x, y, ye.class).size() > 0 && getObjectsAt(x + 1, y - 1, ye.class).size() > 0 && getObjectsAt(x + 2, y - 2, ye.class).size() > 0) {
                         System.out.println("YE hat gewonnen!");
+                        Greenfoot.stop();
+                } 
+                // Überprüft, ob alle Felder voll sind (anhand von der ye Klasse)
+                else if(getObjects(ye.class).size() == 5) {
+                        System.out.println("Untentschieden! Das Spiel wird beendet...");
                         Greenfoot.stop();
                 } 
             }
         }
-        // Funktion überprüft, ob Skete gewonnen hat
+        // Funktionsblock überprüft, ob Skete gewonnen hat
          for(int x = 0; x < 3; x++) {
             for(int y = 0; y < 3; y++) {
+                // Überprüft, ob eine Reihe auf X-Achse voll ist
                 if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x + 1, y, skete.class).size() > 0 && getObjectsAt(x + 2, y, skete.class).size() > 0) {
                         System.out.println("SKETE hat gewonnen!");
                         Greenfoot.stop();
                 } 
-                if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x, y + 1, skete.class).size() > 0 && getObjectsAt(x, y + 2, skete.class).size() > 0) {
+                // Überprüft, ob eine Reihe auf Y-Achse voll ist
+                else if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x, y + 1, skete.class).size() > 0 && getObjectsAt(x, y + 2, skete.class).size() > 0) {
+                        System.out.println("SKETE hat gewonnen!");
+                        mouseclicked++; 
+                        System.out.println(mouseclicked);
+                        Greenfoot.stop();
+                } 
+                // Überprüft, ob eine Reihe quer (XY-Achse) voll ist
+                else if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x + 1, y + 1, skete.class).size() > 0 && getObjectsAt(x + 2, y + 2, skete.class).size() > 0) {
                         System.out.println("SKETE hat gewonnen!");
                         Greenfoot.stop();
                 } 
-                if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x + 1, y + 1, skete.class).size() > 0 && getObjectsAt(x + 2, y + 2, skete.class).size() > 0) {
+                else if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x + 1, y - 1, skete.class).size() > 0 && getObjectsAt(x + 2, y - 2, skete.class).size() > 0) {
                         System.out.println("SKETE hat gewonnen!");
                         Greenfoot.stop();
                 } 
-                if(getObjectsAt(x, y, skete.class).size() > 0 && getObjectsAt(x + 1, y - 1, skete.class).size() > 0 && getObjectsAt(x + 2, y - 2, skete.class).size() > 0) {
-                        System.out.println("SKETE hat gewonnen!");
+                // Überprüft, ob alle Felder voll sind (anhand von der skete Klasse)
+                else if(getObjects(skete.class).size() == 5) {
+                        System.out.println("Untentschieden! Das Spiel wird beendet...");
                         Greenfoot.stop();
                 } 
             }
